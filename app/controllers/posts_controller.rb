3379736a049d41fc78c7763edf727978b4f35c88
Anna <llAnna.Holl@gmail.com>
2013-12-17 11:35:36 +0800

    Add the relation between Post and User

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController <ApplicationController
 	end
 	def create
 		@post = @group.posts.new(post_params)
-
+		@post.author = current_user
 		if @post.save
 			redirect_to group_path(@group)
 		else
@@ -14,11 +14,11 @@ class PostsController <ApplicationController
 		end
 	end
 	def edit
-		@post = @group.posts.find(params[:id])
+		@post = current_user.posts.find(params[:id])
 	end
 
 	def update
-		@post = @group.posts.find(params[:id])
+		@post = @current_user.posts.find(params[:id])
 		if @post.update(post_params)
 			redirect_to group_path(@group)
 		else 
@@ -26,7 +26,7 @@ class PostsController <ApplicationController
 		end	
 	end
 	def destroy
-		@post = @group.posts.find(params[:id])
+		@post = @current_user.posts.find(params[:id])
 		@post.destroy
 		redirect_to groups_path(@group)
 	end
